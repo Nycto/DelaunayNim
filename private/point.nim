75b26605922a5464abe9ffdfd8e828f878774000
Nycto <James@RoundEights.com>
2015-05-11 08:10:25 -0700

    Allow PointList splitting
    This required refactoring PointList so it now
    contains a seq and offsets so that data can be
    shared without copying it around

--- private/point.nim
+++ private/point.nim
@@ -2,8 +2,7 @@
 # Functions for interacting with points
 #
 
-import algorithm
-import sets
+import algorithm, sets, math
 
 #
 # Points represent simple x,y coordinates. The goal is to allow library
@@ -11,7 +10,7 @@ import sets
 # generic type classes
 #
 
-type Point* = concept p
+type Point* = concept p ## \
     ## A type interface for dealing with points
     p.x is float
     p.y is float
@@ -19,15 +18,6 @@ type Point* = concept p
 proc pnt*( x, y: float ): tuple[x, y: float] =
     ## Creates a point
     result = (x, y)
-
-
-#
-# PointList is a phantom type that guarantees a list of points is unique and
-# in sorted order
-#
-
-type PointList*[T] = distinct seq[T]
-    ## A list of sorted, unique points
 
 proc `<=>`*[T: Point]( a, b: T ): int =
     ## Compares two points, sorting left to right, bottom to top
@@ -38,10 +28,30 @@ proc `<=>`*[T: Point]( a, b: T ): int =
     else:
         return cmp(a.y, b.y)
 
+
+#
+# PointList is a phantom type that guarantees a list of points is unique and
+# in sorted order
+#
+
+type PointList*[T] = object ## \
+    ## A list of sorted, unique points
+
+    # The sequence that backs this point list. Passed by reference to avoid
+    # making copies every time a slice is extracted
+    points: seq[T]
+
+    # The starting offset within the backing sequence for this slice
+    start: int
+
+    # The number of points in this slice
+    length: int
+
 proc newPointList*[T: Point]( list: openArray[T] ): PointList[T] =
     ## Creates a point list from a list of points
 
     var output: seq[T] = @[]
+    setlen(output, list.len)
 
     # Keep track of the points that have been seen
     var seen = initSet[tuple[x, y: float]]()
@@ -51,22 +61,55 @@ proc newPointList*[T: Point]( list: openArray[T] ): PointList[T] =
         let pair = (x: point.x, y: point.y)
 
         if not seen.contains(pair):
+            # `add(seq)` pushes on to the end of a sequence, meaning our
+            # `setlen` below would remove the wrong values. We use assignment
+            # instead to avoid that
+            output[seen.len] = point
             seen.incl(pair)
-            output.add(point)
+
+    setlen(output, seen.len)
+    shallow(output)
 
     # Sort points left to right, bottom to top
     output.sort(`<=>`)
 
-    result = PointList(output)
+    result = PointList[T](points: output, start: 0, length: output.len)
 
 proc `@`*[T]( points: PointList[T] ): seq[T] =
     ## Convert a Point list back to a sequence
-    seq[T](points)
+    result = @[]
+    for i in points.start .. <(points.start + points.length):
+        result.add( points.points[i] )
 
-proc len*[T]( points: PointList[T] ): int {.magic: "LengthSeq", noSideEffect.}
+proc len*[T]( points: PointList[T] ): int =
     ## Returns the number of points in a point list
+    points.length
 
-proc `[]`*[T]( s: PointList[T], i: int ): T {.inline.} =
-    seq[T](s)[i]
+proc `[]`*[T]( points: PointList[T], i: int ): T {.inline.} =
+    ## Returns a specific point at the given offset
+    if i >= points.length:
+        raise newException(IndexError, "Index is out of bounds")
+    points.points[i + points.start]
+
+type CantSplitError* = object of Exception ## \
+    ## Thrown when trying to split a list of points that is too small
+
+proc split*[T](
+    points: PointList[T]
+): tuple[left, right: PointList[T]] =
+    ## Divides this PointList evenly in to two smaller lists
+    if len(points) < 4:
+        raise newException(CantSplitError, "PointList is too small to split")
+
+    let halfway: float = points.len / 2
+    let leftLength = toInt(ceil(halfway))
+    let rightLength = toInt(floor(halfway))
+
+    let left = PointList[T](
+        points: points.points, start: points.start, length: leftLength)
+    let right = PointList[T](
+        points: points.points, start: leftLength, length: rightLength)
+
+    return (left: left, right: right)
 
 
--- test/point_test.nim
+++ test/point_test.nim
@@ -36,3 +36,48 @@ suite "PointLists should ":
             ]))
         )
 
+    test "Calculate the length of a point list":
+        let points = newPointList([ pnt(1, 2), pnt(2, 1) ])
+        require( points.len == 2 )
+
+    test "Allow array access to individual values":
+        let points = newPointList([ pnt(1, 2), pnt(2, 1) ])
+        require( points[0] == pnt(1, 2) )
+        require( points[1] == pnt(2, 1) )
+        expect(IndexError):
+            discard points[2]
+
+    test "Split an even point list in half":
+        let points = newPointList([
+            pnt(1, 2), pnt(2, 1), pnt(4, 5), pnt(6, 3) ])
+        let (left, right) = points.split
+        require( @left == @[ pnt(1, 2), pnt(2, 1) ] )
+        require( @right == @[ pnt(4, 5), pnt(6, 3) ] )
+
+    test "Split an odd point list in half":
+        let points = newPointList([
+            pnt(1, 2), pnt(2, 1), pnt(4, 5), pnt(6, 3), pnt(7, 8) ])
+        let (left, right) = points.split
+        require( @left == @[ pnt(1, 2), pnt(2, 1), pnt(4, 5) ] )
+        require( @right == @[ pnt(6, 3), pnt(7, 8) ] )
+
+    test "Throw when trying to split a small list":
+        let points = newPointList([ pnt(1, 2), pnt(2, 1), pnt(4, 5) ])
+        expect(CantSplitError):
+            discard points.split
+
+    test "Allow array access after a split":
+        let points = newPointList([
+            pnt(1, 2), pnt(2, 1), pnt(4, 5), pnt(6, 3) ])
+        let (left, right) = points.split
+
+        require( left[0] == pnt(1, 2) )
+        require( left[1] == pnt(2, 1) )
+        expect(IndexError):
+            discard left[2]
+
+        require( right[0] == pnt(4, 5) )
+        require( right[1] == pnt(6, 3) )
+        expect(IndexError):
+            discard right[2]
+
