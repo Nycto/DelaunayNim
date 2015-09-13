2822b06007f2845fedeca1fe92f3ad53f486f64c
Nycto <James@RoundEights.com>
2015-09-13 11:42:51 -0700

    Fully remove optional_t dependency

--- bin/createSvg.nim
+++ bin/createSvg.nim
@@ -1,14 +1,17 @@
-import strutils, delaunay, optional_t
+import strutils, delaunay, options
 
 when defined(profiler) or defined(memProfiler):
     import nimprof
+
+proc getOrElse[T]( opt: Option[T], default: T ): T =
+    if opt.isSome: opt.get else: default
 
 var points: seq[tuple[x, y: float]] = @[]
 
 var width: float = 0
 var height: float = 0
-var minX: Option[float] = None[float]()
-var minY: Option[float] = None[float]()
+var minX: Option[float] = none(float)
+var minY: Option[float] = none(float)
 
 for line in stdin.lines:
     let parts = line.split({' ', ','})
@@ -25,12 +28,12 @@ for line in stdin.lines:
             # Track the highest and lowest `X` to snug in the viewport
             width = if point.x > width: point.x else: width
             if minY.isNone or point.x < minX.get:
-                minX = Some(point.x)
+                minX = some(point.x)
 
             # Track the highest and lowest `Y` to snug in the viewport
             height = if point.y > height: point.y else: height
             if minY.isNone or point.y < minY.get:
-                minY = Some(point.y)
+                minY = some(point.y)
 
         except ValueError:
             stderr.writeln "Point can not be parsed as numbers:\n" & line
@@ -39,13 +42,13 @@ for line in stdin.lines:
 
 echo "<?xml version=\"1.0\"?>"
 echo "<svg width=\"$1\" height=\"$2\"" % [
-    $(width - minX.get(0)), $(height - minY.get(0)) ]
+    $(width - minX.getOrElse(0)), $(height - minY.getOrElse(0)) ]
 echo "    version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">"
 
 for a, b in triangulate(points):
     echo "  <line x1=\"$1\" y1=\"$2\" x2=\"$3\" y2=\"$4\"" % [
-        $(a.x - minX.get(0)), $(height - a.y),
-        $(b.x - minX.get(0)), $(height - b.y)
+        $(a.x - minX.getOrElse(0)), $(height - a.y),
+        $(b.x - minX.getOrElse(0)), $(height - b.y)
     ]
     echo "    stroke=\"black\" stroke-width=\"2\"/>"
 
